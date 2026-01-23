0fa14ece3973a41ce506a776f72befa5a3523a60
cn-guy <cindynguyen7544@gmail.com>
2026-01-22 16:46:24 -0800

    Progress - Preprocessor cleaning data

--- db_pull.py
+++ db_pull.py
@@ -5,6 +5,8 @@ import json
 
 BASE_URL = "http://vocadb.net/api/"
 
+### Helper Methods ###
+
 # Load a .json file
 def load_data(json_file):
     with open(json_file, "r", encoding="utf-8") as f:
@@ -20,7 +22,7 @@ def get_single(endpoint, params=None):
     data = r.json()
     return data
 
-
+# Retrieve the data from an endpoint
 def get_data(endpoint, params, max_results=100, all=False):
     data = []
     url = BASE_URL + endpoint
@@ -58,6 +60,7 @@ def get_data(endpoint, params, max_results=100, all=False):
     print(len(data))
     return data
 
+# Pull all songlist fields
 def extract_songlist_fields(sl_data):
     sl_ids = []
     for sl in sl_data:
@@ -65,13 +68,12 @@ def extract_songlist_fields(sl_data):
 
     return sl_ids
 
-
+# Write the data to a .json file
 def write_data(data, filename):
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=2)
 
-
-
+# Get ALL songlists
 def get_all_songlists():
     params = {
         "featuredCategory": "Concerts",
@@ -79,8 +81,8 @@ def get_all_songlists():
     data = get_data("songLists/featured", params=params, max_results=100, all=True)
     return data
 
-
-def get_songlist_songs():
+# Get data on ALL setlists and all their songs (NOT UNIQUE)
+def get_songlist_songs(clean=False):
     data = get_all_songlists()
     ex_data = extract_songlist_fields(data)
     sl_song_data = []
@@ -90,20 +92,34 @@ def get_songlist_songs():
             "fields" : "Names"
         }
         sl_data = get_data(f"songLists/{sl_id}/songs", params=params, max_results=100, all=True)
-        sl_song_data.extend(sl_data)
-        if len(sl_data) > 50:
-            print(sl_id)
 
+        if clean == True and len(sl_data) > 50:
+            continue
+        else:
+            sl_song_data.extend(sl_data)
 
-    write_data(sl_song_data,"sl_song_data.json")
+    if clean == True:
+        write_data(sl_song_data, "CLEAN_sl_song_data.json")
+    else:
+        write_data(sl_song_data,"sl_song_data.json")
 
-def build_songs_dataset():
+# Build list of unique songs and compute their average order in a setlist
+def build_songs_dataset(clean=False):
     sl_song_data = load_data("sl_song_data.json")
+
+    if clean == True:
+        sl_song_data = load_data("CLEAN_sl_song_data.json")
+
     unique_sdata = {}
     sdata_list = []
     for s in sl_song_data:
-        id = s["song"]["id"]
         order = s["order"]
+
+        if(clean == True):
+            if order > 50:
+                continue
+
+        id = s["song"]["id"]
 
         if id not in unique_sdata:
             s["count"] = 1
@@ -118,14 +134,19 @@ def build_songs_dataset():
             c = song["count"]
             song["avg_order"] += (order - song["avg_order"])/c
 
-
-
 #    sdata_list.sort(key = lambda d: d["count"])
-    write_data(sdata_list, "unique_setlist_songs.json") 
 
+    if clean == True:
+        write_data(sdata_list, "CLEAN_unique_setlist_songs.json")
+    else:
+        write_data(sdata_list, "unique_setlist_songs.json") 
 
-def build_simplified_dataset():
+# Create dataset that will be used for the rest of the project
+def build_simplified_dataset(clean=False):
     full_data_list = load_data("unique_setlist_songs.json")
+   
+    if clean == True:
+        full_data_list = load_data("CLEAN_unique_setlist_songs.json")
 
     simple_data_list = []
     for song_data in full_data_list:
@@ -166,13 +187,23 @@ def build_simplified_dataset():
         simple_data_list.append(data)
     return simple_data_list
 
+
+# Main
 if __name__ == "__main__":
-    get_songlist_songs()
-    build_songs_dataset()
+    # Retrieve all basic data (this is necessary for the cleaned data steps)
+    #get_songlist_songs()
+    #build_songs_dataset()
     #data = load_data("unique_setlist_songs.json")
-    #print(len(data))
-    data = build_simplified_dataset()
-    write_data(data, "vocaloid_setlist_songs.json")
+    #print(len(data)) # Test print
+    #data = build_simplified_dataset()
+    #write_data(data, "vocaloid_setlist_songs.json")
+
+    # Retrieve cleaned preprocessor data (DO NOT include any songs where order > 50)
+    get_songlist_songs(clean=True)
+    build_songs_dataset(clean=True)
+    data = load_data("CLEAN_unique_setlist_songs.json")
+    data = build_simplified_dataset(clean=True)
+    write_data(data, "CLEAN_vocaloid_setlist_songs.json")
 
 
 
